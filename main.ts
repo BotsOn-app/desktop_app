f11ed08b639bfe6ce8a7a0c47992970124a8b87f
Nirbose <nirbose2@gmail.com>
2022-04-02 12:05:55 +0200

    ipc test

--- main.ts
+++ main.ts
@@ -1,9 +1,14 @@
-import { BrowserWindow, app, ipcMain, ipcRenderer } from 'electron';
+import { BrowserWindow, app } from 'electron';
+import { ipcMain } from 'electron/main';
+import * as fs from 'fs';
 
 const createMainWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      preload: __dirname + '\\preload.js'
+    }
   })
 
   win.loadFile('build/index.html')
@@ -28,4 +33,31 @@ app.whenReady().then(() => {
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
+})
+
+// Add e new discord bot in appdata folder for save the token and extensions
+ipcMain.on('new-bot', (event, token) => {
+  console.log(token)
+  let appData = app.getPath("userData")
+  console.log(appData)
+  let bot = {
+    token: token,
+    extensions: []
+  }
+  // verif bots.json exist
+  if (!fs.existsSync(appData + "\\bots.json")) {
+    fs.writeFileSync(appData + "\\bots.json", JSON.stringify([bot]))
+  }
+
+  let bots = require(appData + "/bots.json")
+  bots.push(bot)
+  fs.writeFileSync(appData + "/bots.json", JSON.stringify(bots))
+  // event.sender.send('bot-added', bot)
+})
+
+// Get all bots in appdata folder
+ipcMain.on('get-bots', (event) => {
+  let appData = app.getPath("userData")
+  let bots = require(appData + "/bots.json")
+  // event.sender.send('bots-loaded', bots)
 })
