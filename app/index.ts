dc1011aa97c6a2b381ff99d84b7c123e8680bedc
5CYTH3 <ScytheD@outlook.fr>
2022-05-20 20:02:01 +0200

    Added new pacakges to support Typescript.

--- app/index.ts
+++ app/index.ts
@@ -1,12 +1,12 @@
-const { app, BrowserWindow } = require('electron');
-const { join } = require('path');
+const { app, BrowserWindow } = require("electron");
+const { join } = require("path");
 
-app.whenReady().then(main)
+app.whenReady().then(main);
 
 function main() {
-    const window = new BrowserWindow({
-        width: 800,
-        height: 650
-    })
-    window.loadFile(join(__dirname, '/public/index.html'));
+	const window = new BrowserWindow({
+		width: 800,
+		height: 650,
+	});
+	window.loadFile(join(__dirname, "../public/index.html"));
 }
--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
-import sveltePreprocess from 'svelte-preprocess';
+import autoPreprocess from 'svelte-preprocess';
 import typescript from '@rollup/plugin-typescript';
 import css from 'rollup-plugin-css-only';
 
@@ -40,21 +40,13 @@ export default {
 	},
 	plugins: [
 		svelte({
-			preprocess: sveltePreprocess({ sourceMap: !production }),
+			preprocess: autoPreprocess(),
 			compilerOptions: {
 				// enable run-time checks when not in production
 				dev: !production
 			}
 		}),
-		// we'll extract any component CSS out into
-		// a separate file - better for performance
 		css({ output: 'bundle.css' }),
-
-		// If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration -
-		// consult the documentation for details:
-		// https://github.com/rollup/plugins/tree/master/packages/commonjs
 		resolve({
 			browser: true,
 			dedupe: ['svelte']
@@ -65,16 +57,8 @@ export default {
 			inlineSources: !production
 		}),
 
-		// In dev mode, call `npm run start` once
-		// the bundle has been generated
 		!production && serve(),
-
-		// Watch the `public` directory and refresh the
-		// browser on changes when not in production
 		!production && livereload('public'),
-
-		// If we're building for production (npm run build
-		// instead of npm run dev), minify
 		production && terser()
 	],
 	watch: {
