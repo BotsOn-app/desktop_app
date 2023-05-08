bd28c66e17edfcafb2c0444c08d826819e35b4f7
5CYTH3 <ScytheD@outlook.fr>
2023-05-08 14:26:34 +0200

    Updated dependencies + added a .env.example

--- src/routes/home/+page.ts
+++ src/routes/home/+page.ts
@@ -1,13 +1,13 @@
-import type {Load} from "@sveltejs/kit";
-import {env} from "$env/dynamic/public";
+import type { Load } from '@sveltejs/kit';
+import { env } from '$env/dynamic/public';
 
 export const load: Load = async ({ fetch }) => {
-    let res = await fetch(`${env.PUBLIC_API_URL}`);
-    let data = await res.json();
-    return {
-        author:  {
-            name: "Saimogu",
-            extensions: []
-        }
-    }
-}
+	let res = await fetch(`${env.PUBLIC_API_URL}`);
+	let data = await res.json(); // For now data is useless as we don't need to fetch anything but it acts like a placeholder
+	return {
+		author: {
+			name: 'Saimogu',
+			extensions: [],
+		},
+	};
+};
