75a6690b43a8809a44c89bbedd9430e7074d003f
5CYTH3 <ScytheD@outlook.fr>
2023-05-27 17:56:50 +0200

    Just added filewriting, nothing too important

--- src-tauri/src/bot.rs
+++ src-tauri/src/bot.rs
@@ -1,3 +1,5 @@
+use std::fs::File;
+use std::io::Write;
 pub struct Bot {
     token: String,
     path: String,
@@ -24,10 +26,16 @@ impl Bot {
         self.extensions.push(extension);
     }
 
-    fn register() {
+    fn register() -> std::io::Result<()> {
+        let mut index = File::create("index.js")?;
+        match index.write_all(r#""#.as_bytes()) {
+            Ok(val) => (),
+            Err(e) => return Err(e),
+        };
         // Here we register the bot by creating its folders and the base index.js.
         // Cf this SO post about how to modify a file content (here to add new extensions to the file).
         // https://stackoverflow.com/questions/50689785/how-do-you-modify-a-files-contents-instead-of-prepending-to-the-file-in-rust
         // Before doing anything here I need to define the architecture and make a graph.
+        Ok(())
     }
 }
