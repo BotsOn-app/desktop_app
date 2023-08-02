d21f0c1b91f4f7a1a1a726864ebaa4336b5cc085
5CYTH3 <ScytheD@outlook.fr>
2023-08-02 13:08:44 +0200

    Fixed a little bug due to a bad merge conflict resolving

--- src/lib/components/home/BotCard.svelte
+++ src/lib/components/home/BotCard.svelte
@@ -1,17 +1,21 @@
 <script lang="ts">
+	import { invoke } from '@tauri-apps/api/tauri';
 	import ActionButton from '../utils/ActionButton.svelte';
 	import Text from '../utils/Text.svelte';
-	import { goto } from '$app/navigation';
 	import { FastAverageColor } from 'fast-average-color';
-	export let imgUrl: string;
-	export let name: string;
 
-	const fac = new FastAverageColor();
+	export let bot: any;
 
 	const getDominantColor = async () => {
-		let _color = await fac.getColorAsync(imgUrl);
+		const fac = new FastAverageColor();
+
+		let _color = await fac.getColorAsync(bot.avatarUrl);
 		return _color.hex;
 	};
+
+	const deleteBot = () => invoke('delete_bot', { token: bot.token })
+
+	
 </script>
 
 <div
@@ -19,22 +23,17 @@
 >
 	<div class="flex flex-row justify-center items-center">
 		<!-- Bot picture -->
-		{#await getDominantColor()}
-			<div class="relative">
-				<div class="absolute blur-2xl h-12 w-12 top-0 left-0 bg-gray-500" />
-				<div class="relative h-12 w-12 bg-gray-500 mr-4 rounded-full z-10 animate-pulse"></div>
-			</div>
-		{:then color}
+		{#await getDominantColor() then color}
 			<div class="relative">
 				<div class="absolute blur-2xl h-12 w-12 top-0 left-0" style="background-color: {color};" />
-				<img src={imgUrl} alt="" class="relative h-12 w-12 bg-gray-500 mr-4 rounded-full z-10" />
+				<img src={bot.avatarUrl} alt="" class="relative h-12 w-12 bg-red-600 mr-4 rounded-full z-10" />
 			</div>
 		{/await}
 		<!-- Bot Name -->
 		<Text class="font-bold">{bot.name}</Text>
 	</div>
 	<div class="flex flex-row justify-around items-center w-44">
-		<ActionButton buttonType="danger" action={() => {}}>
+		<ActionButton buttonType="danger" action={() => deleteBot()}>
 			<box-icon type="solid" name="trash" color="white" />
 		</ActionButton>
 		<ActionButton buttonType="info" action={() => {}}>
@@ -45,3 +44,4 @@
 		</ActionButton>
 	</div>
 </div>
+
