67b4794ad091358083d2df437b70ae8067ae9a8e
Firelop <firelop2contact@gmail.com>
2023-09-09 19:29:30 +0200

    Resolved Issue#27 by dynamically importing modules from tauri api in src/routes/+page.ts and creating "data.json" file on the first launch.

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,13 +1,20 @@
-import { pageSelector } from '$lib/functions/stores';
-import { appDataDir, resolveResource } from '@tauri-apps/api/path';
-import { readTextFile } from '@tauri-apps/api/fs'
+const { pageSelector } = await import('$lib/functions/stores');
+
 
 export const load = async () => {
+	const { appDataDir} = await import('@tauri-apps/api/path');
+	const { readTextFile, writeTextFile, exists } = await import('@tauri-apps/api/fs');
+
 	pageSelector.update((n) => 0);
 
 	const newAppDataDir = await appDataDir();
 
-	const path: string = newAppDataDir + "bots\\data.json"
+	const path: string = newAppDataDir + "bots\\data.json";
+
+	if (!(await exists(path))) {
+		await writeTextFile(path, '[]');
+	}
+
 	const json = JSON.parse(await readTextFile(path));
 	return {
 		bots: json,
